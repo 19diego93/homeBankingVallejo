4b6bd7962c851fd74611df136b418bcd2fa7a77f
diego vallejo <vallejodiego@live.com>
2024-04-30 10:41:59 -0300

    sprint 1 homebanking

---
+++ homebanking/homebanking/src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,52 @@
+package com.mindhub.homebanking.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Client() {
+    }
+
+    public Client(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdCard() {
+        return email;
+    }
+
+    public void setIdCard(String email) {
+        this.email = email;
+    }
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
